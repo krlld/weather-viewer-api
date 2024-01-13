f12ff0ad84c1c0b3752a07e9a3d0ddd223f9e01b
krlld <kirilldikun25@gmail.com>
2024-01-13 19:22:57 +0300

    feat: add bcrypt to authenticate service

---
+++ src/main/java/by/kirilldikun/weatherviewerapi/auth/exception/UserNotVerifiedException.java
@@ -0,0 +1,8 @@
+package by.kirilldikun.weatherviewerapi.auth.exception;
+
+public class UserNotVerifiedException extends RuntimeException {
+
+    public UserNotVerifiedException(String message) {
+        super(message);
+    }
+}
--- src/main/java/by/kirilldikun/weatherviewerapi/auth/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/by/kirilldikun/weatherviewerapi/auth/service/impl/AuthenticationServiceImpl.java
@@ -1,5 +1,7 @@
 package by.kirilldikun.weatherviewerapi.auth.service.impl;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import by.kirilldikun.weatherviewerapi.auth.exception.UserNotVerifiedException;
 import by.kirilldikun.weatherviewerapi.auth.model.Session;
 import by.kirilldikun.weatherviewerapi.auth.model.User;
 import by.kirilldikun.weatherviewerapi.auth.service.AuthenticationService;
@@ -10,19 +12,25 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
 
+    private static final int COST = 10;
+
     private final UserService userService;
 
     private final SessionService sessionService;
 
     @Override
     public Session register(User user) {
+        user.setPassword(BCrypt.withDefaults().hashToString(COST, user.getPassword().toCharArray()));
         userService.save(user);
         return sessionService.create(user);
     }
 
     @Override
     public Session authenticate(User user) {
-        user = userService.findByEmail(user.getEmail());
-        return sessionService.create(user);
+        User databaseUser = userService.findByEmail(user.getEmail());
+        if (!BCrypt.verifyer().verify(user.getPassword().toCharArray(), databaseUser.getPassword()).verified) {
+            throw new UserNotVerifiedException("User with email: " + user.getEmail() + " not verified");
+        }
+        return sessionService.create(databaseUser);
     }
 }
